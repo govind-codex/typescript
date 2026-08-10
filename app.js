45b0502dd8f217d5fdfe014b996bac81dff226f3
govind-codex <nagargovind902@gmail.com>
2026-08-10 16:01:05 +0530

    datatypes

---
+++ app.js
@@ -0,0 +1,11 @@
+"use strict";
+// Datatypes in typescript
+// primitive types (number, string, boolean)
+// reference types ([], {}, ())
+// Array, tuples, enums
+// any, unknown, void, null, undefined, never
+Object.defineProperty(exports, "__esModule", { value: true });
+let a; //define that type of a is number 
+a = 35;
+let b; //type any cause we didn't define the typeof b
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,9 @@
+// Datatypes in typescript
+// primitive types (number, string, boolean)
+// reference types ([], {}, ())
+// Array, tuples, enums
+// any, unknown, void, null, undefined, never
+
+let a: number; //define that type of a is number 
+a= 35;
+let b; //type any cause we didn't define the typeof b
