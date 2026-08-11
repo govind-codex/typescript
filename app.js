2f5fbb8249a90ab9f4766fabf94d308c47e619b3
govind-codex <nagargovind902@gmail.com>
2026-08-11 14:02:50 +0530

    class, objects, constructor, this keyword

--- app.js
+++ app.js
@@ -4,8 +4,56 @@
 // reference types ([], {}, ())
 // Array, tuples, enums
 // any, unknown, void, null, undefined, never
-Object.defineProperty(exports, "__esModule", { value: true });
-let a; //define that type of a is number 
-a = 35;
-let b; //type any cause we didn't define the typeof b
+// Object.defineProperty(exports, "__esModule", { value: true });
+// let a: number; //define that type of a is number 
+// a= 35;
+// let b; //type any cause we didn't define the typeof b
+//type inference
+//understanding type inference
+// function abcd(a: number, b: string): void {}    //type annotation
+//interface and types
+// interface user{
+//     name?: string, optional
+//     email: string,
+//     password: string
+// }
+// function getDataOfUser(obj: user) {}
+// getDataOfUser({email: "test@gmail.com", password: "test"})
+//extending interface
+// interface admin extends user{admin: boolean;}
+//type aliases
+// type value= number | string | null;
+// function cd(obj: value){}
+//union(|) and intersection(&) types
+//class - factory of objects
+class Device {
+    name = "lg";
+    price = 1200;
+    category = "electronics";
+}
+let d1 = new Device();
+let d2 = new Device();
+//constructor - maker
+// define public is also imp
+class HumanMaker {
+    name;
+    isHandsome;
+    age = 0;
+    constructor(name, isHandsome) {
+        this.name = name;
+        this.isHandsome = isHandsome;
+    }
+}
+let b1 = new HumanMaker('govind', true);
+let b2 = new HumanMaker('harsh', false);
+// this keyword in objects & class
+//this - if we access class's variable in any kind of function under class then you have to use this 
+class bottleMaker {
+    name;
+    constructor(name) {
+        this.name = name;
+        this.name = name;
+    }
+}
+let b3 = new bottleMaker('coca cola');
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -4,6 +4,59 @@
 // Array, tuples, enums
 // any, unknown, void, null, undefined, never
 
-let a: number; //define that type of a is number 
-a= 35;
-let b; //type any cause we didn't define the typeof b
+// let a: number; //define that type of a is number 
+// a= 35;
+// let b; //type any cause we didn't define the typeof b
+
+//type inference
+//understanding type inference
+// function abcd(a: number, b: string): void {}    //type annotation
+
+//interface and types
+// interface user{
+//     name?: string, optional
+//     email: string,
+//     password: string
+// }
+
+// function getDataOfUser(obj: user) {}
+// getDataOfUser({email: "test@gmail.com", password: "test"})
+
+//extending interface
+// interface admin extends user{admin: boolean;}
+
+//type aliases
+// type value= number | string | null;
+
+// function cd(obj: value){}
+
+//union(|) and intersection(&) types
+//class - factory of objects
+class Device {
+    name = "lg";
+    price = 1200;
+    category = "electronics";
+}
+let d1 = new Device();
+let d2 = new Device();
+
+//constructor - maker
+// define public is also imp
+class HumanMaker{
+    age=0;
+    constructor(public name: string, public isHandsome: boolean){
+
+    }
+}
+let b1 = new HumanMaker('govind', true)
+let b2 = new HumanMaker('harsh', false)
+
+// this keyword in objects & class
+//this - if we access class's variable in any kind of function under class then you have to use this 
+class bottleMaker {
+    constructor(public name: string) {
+        this.name = name;
+    }
+}
+
+let b3 = new bottleMaker('coca cola');
